042c1516ab225d13782c0125c22dd5229198c866
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-02-24 15:59:16 +0300

    get user by id request has been added

--- app/routers/user.py
+++ app/routers/user.py
@@ -16,4 +16,10 @@ def get_users(db: Session = Depends(get_db)):
                             detail=f"Users has not found.")
     return users
 
-    
+@router.get('/users/{id}', response_model=list[schemas.UserOut])
+def get_user(db: Session = Depends(get_db)):
+    users = db.query(models.User).filter(models.User.id == id).first()
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Users has not found.")
+    return users
