6a0c9326c715d9dd213f46f8266db8dcb7fe28fd
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-02-26 10:04:37 +0300

    сделал датабейз юрл ос переменной

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1990082dsg1212@localhost/fastapi'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("PG_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
