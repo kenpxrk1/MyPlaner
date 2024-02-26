6309757fae6e457c852c2307f87d6d71ea9574a7
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-02-26 10:20:59 +0300

    model for tasks has been created success

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ from sqlalchemy import *
 
 from app.database import Base
 
+class Tasks(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, 
+                        server_default=text('now()'))
+    creator_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE"), nullable=False)
+
 class User(Base):
     __tablename__ = 'Users'
 
