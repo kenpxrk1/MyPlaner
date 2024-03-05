f18ac9c2689ba4d2d67470bfde48c8b739965c7d
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-03-05 20:52:21 +0300

    some bugs fixed

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -23,6 +23,5 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
 
     return {
         "access_token": access_token,
-        "refresh_token": refresh_token,
         "token_type": "bearer"
     }
