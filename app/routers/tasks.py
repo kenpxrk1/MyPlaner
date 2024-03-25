f93b87ca948680ae257186fdc08e8b9235782d9f
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-03-25 09:15:07 +0300

    a lot get endpoints for tasks added

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,19 +1,69 @@
+import datetime
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import extract
 from .. import schemas, database, models,  utils, oauth2
 from ..database import get_db
+
+"""CONST DATETIME VARIABLE BLOCK"""
+
+today_date = datetime.datetime.today().strftime('%Y-%m-%d')
+current_month_obj = datetime.datetime.strptime(today_date, '%Y-%m-%d')
+current_month = current_month_obj.month
+
+
+"""CONST DATETIME VARIABLE BLOCK"""
+
+
 
 router = APIRouter(
     tags=['Tasks'],
     prefix="/tasks"
 )
 
-""" TAKE AUTH USER OWN TASKS ONLY (ALL) """
+""" TAKE AUTH USER OWN TASKS ONLY (TODAY)) """
 
-@router.get('/', response_model=List[schemas.Task])
-def get_tasks(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+@router.get('/today', response_model=List[schemas.Task])
+def get_today_tasks(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    tasks = db.query(models.Tasks).filter(models.Tasks.owner_id == current_user.id).filter(models.Tasks.starts_at == today_date).all()
+
+    if tasks is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tasks does not exits")
+    
+    return tasks
+
+""" TAKE AUTH USER OWN TASKS (ALL) """
+
+@router.get('/all', response_model=List[schemas.Task])
+def get_all_tasks(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     tasks = db.query(models.Tasks).filter(models.Tasks.owner_id == current_user.id).all()
+
+    if tasks is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tasks does not exits")
+    
+    return tasks
+
+""" TAKE AUTH USER OWN TASKS (BY CURRENT MONTH) """
+
+@router.get('/current_month', response_model=List[schemas.Task])
+def get_current_month_tasks(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    tasks = db.query(models.Tasks).filter(models.Tasks.owner_id == current_user.id).filter(extract(
+        'month', models.Tasks.starts_at) == current_month).all()
+
+    if tasks is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tasks does not exits")
+    
+    return tasks
+
+""" TAKE AUTH USER OWN TASKS (BY MONTH) """
+
+@router.get('/month', response_model=List[schemas.Task])
+def get_month_tasks(date_query: datetime.date, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    query_month_obj = datetime.datetime.strptime(date_query, '%Y-%m-%d')
+    query_month = query_month_obj.month
+    tasks = db.query(models.Tasks).filter(models.Tasks.owner_id == current_user.id).filter(extract(
+        'month', models.Tasks.starts_at) == query_month).all()
 
     if tasks is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tasks does not exits")
